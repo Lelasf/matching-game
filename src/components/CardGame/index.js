573fbec0e134a3af86d0c5bce53184344634f9a6
Leonardo Ferrari <leoferrarifranca@hotmail.com>
2022-02-16 10:09:11 -0300

    create project base

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame() {
+  return `
+  <article class='card-game'>
+    <img src="src/images/logo-alura.svg" alt="Logo da Alura">
+  </article>
+  `
+}
+
+export default CardGame
