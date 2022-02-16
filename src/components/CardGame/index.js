c54548f38f797def19721f8104dc4563204521c1
Leonardo Ferrari <leoferrarifranca@hotmail.com>
2022-02-16 10:54:17 -0300

    Create ScoreBoard object

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,7 +1,7 @@
 import './style.css'
 
 function CardGame() {
-  return `
+  return /*html*/ `
   <article class='card-game'>
     <img src="src/images/logo-alura.svg" alt="Logo da Alura">
   </article>
