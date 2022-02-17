8055179c7e1f74d22a5efa8444e2bbaab5fedaa5
Leonardo Ferrari <leoferrarifranca@hotmail.com>
2022-02-17 13:58:53 -0300

    Create Arrow and Points components

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './src/styles/settings/colors.css'
 import './src/styles/generic/reset.css'
 import './src/styles/elements/base.css'
-import ScoreBoard from './src/objects/ScoreBoard/INDEX.JS'
+import ScoreBoard from './src/objects/ScoreBoard/index.js'
 import BoardGame from './src/objects/BoardGame'
 
 const $root = document.querySelector('#root')
---
+++ src/components/GamePoints/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+function GamePoints() {
+  return /*html*/ `
+      <div class='space'></div>
+
+    `
+}
+
+export default GamePoints
