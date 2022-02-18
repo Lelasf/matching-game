47ded0867b445011719acdf035c222367f1bd343
Leonardo Ferrari <leoferrarifranca@hotmail.com>
2022-02-18 13:49:07 -0300

    Create ArrowDown component

---
+++ src/components/ArrowDown/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function ArrowDown() {
+  return /*html*/ `
+    <div class='arrow-down'>
+      <img src='src/images/IconArrowDown.svg' alt='Seta'>
+    </div>
+  `
+}
+
+export default ArrowDown
